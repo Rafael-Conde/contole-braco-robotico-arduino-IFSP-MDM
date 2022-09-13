ee141fbb93cd8ad4b62907206086398dce2c3089
Rafael de Conde <GitHubRafael@gmail.com>
2022-09-13 17:07:14 -0300

    adicionou-se a funcionalidade de contolar a garra com o botão

--- controle_braco_robotico.ino
+++ controle_braco_robotico.ino
@@ -6,7 +6,7 @@
 #define pinServ1 2
 #define pinServ2 3
 #define pinServ3 4
-// #define pinServ4 5
+#define pinServ4 5
 
 // define as portas dos potenciometros
 #define pot1 A0
@@ -19,6 +19,8 @@ Servo serv1,serv2,serv3,serv4;
 
 // cria as variavies dos angulos de cada motor
 int motor1,motor2,motor3,motor4;
+int pino_botao{6};
+bool botao_apertado{false};
 
 unsigned long mostradorTimer = 1;
 const unsigned long intervaloMostrador = 5000;
@@ -27,13 +29,14 @@ void setup() {
 
   //inicia o monitor serial
   Serial.begin(9600); 
-
+  pinMode(pino_botao, INPUT_PULLUP);
   // atribui pinos dos servos
   serv1.attach(pinServ1);
   serv2.attach(pinServ2);
   serv3.attach(pinServ3);
-  // serv4.attach(pinServ4);
-  
+  serv4.attach(pinServ4);
+  serv4.write(50);
+
 }
 
 void loop(){
@@ -48,34 +51,43 @@ void loop(){
   serv1.write(motor1);
   serv2.write(motor2);
   serv3.write(motor3);
+  if(!digitalRead(pino_botao))
+  {
+    botao_apertado = !botao_apertado;
+    if(botao_apertado)
+    {
+      serv4.write(0); 
+    }
+    else
+    {
+      serv4.write(50);
+    }
+  }
   // serv4.write(motor4);
 
-   if ((millis() - mostradorTimer) >= intervaloMostrador) {
 
-  // envio para o monitor serial do posicionamentos dos motores
-  Serial.println("**********************************************");
-  
-  Serial.print("Pot1:");
-  Serial.print(analogRead(pot1));
-  Serial.print(" Angulo Motor1:");
-  Serial.println(motor1);
+  if ((millis() - mostradorTimer) >= intervaloMostrador) {
 
-  Serial.print("Pot2:");
-  Serial.print(analogRead(pot2));
-  Serial.print(" Angulo Motor2:");
-  Serial.println(motor2);
+    // envio para o monitor serial do posicionamentos dos motores
+    Serial.println("**********************************************");
 
-  Serial.print("Pot3:");
-  Serial.print(analogRead(pot3));
-  Serial.print(" Angulo Motor3:");
-  Serial.println(motor3);
+    Serial.print("Pot1:");
+    Serial.print(analogRead(pot1));
+    Serial.print(" Angulo Motor1:");
+    Serial.println(motor1);
 
-  // Serial.print("Pot4:");
-  // Serial.print(analogRead(pot4));
-  // Serial.print(" Angulo Motor4:");
-  // Serial.println(motor4);
+    Serial.print("Pot2:");
+    Serial.print(analogRead(pot2));
+    Serial.print(" Angulo Motor2:");
+    Serial.println(motor2);
 
-  mostradorTimer = millis();
+    Serial.print("Pot3:");
+    Serial.print(analogRead(pot3));
+    Serial.print(" Angulo Motor3:");
+    Serial.println(motor3);
+
+
+    mostradorTimer = millis();
   }
 
   // tempo de espera para recomeçar
